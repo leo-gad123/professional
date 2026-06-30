55b8ebf8ec4cc9a27792dcb963f2e0969f69c6e9
leo-gad123 <hakizimanaleogad@gmail.com>
2026-06-30 15:20:26 +0200

    Fix DB connection await in serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
 import app from "../server/index.js";
 import { connectDB } from "../server/db.js";
 
-connectDB();
+let dbPromise: Promise<void> | null = null;
+
+app.use(async (_req: Request, _res: Response, next: NextFunction) => {
+  if (!dbPromise) {
+    dbPromise = connectDB();
+  }
+  await dbPromise;
+  next();
+});
 
 export default app;
